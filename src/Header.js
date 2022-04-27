f3fb63f0c467b4b5ea35c2c8d043049ffb18e3a5
Kenneth Tran <48599140+kennethtran590@users.noreply.github.com>
2022-04-26 22:24:40 -0700

    Installed MaterialUI, initialized search bar Header component and styling, Added menu icon, gmail image, and

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,30 @@
 import React from "react";
 import "./Header.css";
+import MenuIcon from "@mui/icons-material/Menu";
+import { IconButton } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+
 function Header() {
   return (
     <div className="header">
-      <h1>This is a header</h1>
+      <div className="header__left">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <img
+          src="https://i.pinimg.com/originals/ae/47/fa/ae47fa9a8fd263aa364018517020552d.png"
+          alt=""
+        />
+      </div>
+
+      <div className="header__middle">
+        <SearchIcon />
+        <input placeholder="Search mail" type="text" />
+        <ArrowDropDownIcon />
+      </div>
+
+      <div className="header__right" className="header__inputCaret"></div>
     </div>
   );
 }
