63efd0954dd7cb60b7984bad7e268e86a9b660e1
Kenneth Tran <48599140+kennethtran590@users.noreply.github.com>
2022-04-26 22:11:28 -0700

    Initialize Header component and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="app">
-      <h1>Lets build Gmail</h1>
+      <Header />
+      <h1>Lets Build Gmail</h1>
     </div>
   );
 }
