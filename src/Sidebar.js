eafe3726ba259326cb512554f72b784e5ca42744
Kenneth Tran <48599140+kennethtran590@users.noreply.github.com>
2022-04-30 12:06:31 -0700

    Imported Button component from MaterialUI, added Compose button component to sidebar component

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,8 +1,18 @@
+import { Button } from "@mui/material";
 import React from "react";
 import "./Sidebar.css";
+import AddIcon from "@mui/icons-material/Add";
 
 function Sidebar() {
-  return <div className="sidebar"></div>;
+  return (
+    <div className="sidebar">
+      <Button
+        startIcon={<AddIcon fontSize="large" className="sidebar__compose" />}
+      >
+        Compose
+      </Button>
+    </div>
+  );
 }
 
 export default Sidebar;
