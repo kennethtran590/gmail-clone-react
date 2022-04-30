333d767f900055c42aa641d45286e080752d8b1f
Kenneth Tran <48599140+kennethtran590@users.noreply.github.com>
2022-04-30 14:29:49 -0700

    Finished implementing the Compose button component with styling

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,15 +2,20 @@ import { Button } from "@mui/material";
 import React from "react";
 import "./Sidebar.css";
 import AddIcon from "@mui/icons-material/Add";
+import InboxIcon from "@mui/icons-material/Inbox";
+import SidebarOption from "./SidebarOption";
 
 function Sidebar() {
   return (
     <div className="sidebar">
       <Button
-        startIcon={<AddIcon fontSize="large" className="sidebar__compose" />}
+        fontSize="large"
+        className="sidebar__compose"
+        startIcon={<AddIcon />}
       >
         Compose
       </Button>
+      <SidebarOption Icon={InboxIcon} title="Inbox" number={54} />
     </div>
   );
 }
