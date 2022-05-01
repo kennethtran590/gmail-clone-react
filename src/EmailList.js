257c8d18ab1d5d2cab1a84b57e75c9f614a1a2fc
Kenneth Tran <48599140+kennethtran590@users.noreply.github.com>
2022-05-01 15:25:34 -0700

    Added all icons for email list settings left hand side

--- src/EmailList.js
+++ src/EmailList.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./EmailList.css";
 import { Checkbox, IconButton } from "@mui/material";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import RedoIcon from "@mui/icons-material/Redo";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 
 function EmailList() {
   return (
@@ -8,6 +11,15 @@ function EmailList() {
       <div className="emailList_settings">
         <div className="emailList__settingsLeft">
           <Checkbox />
+          <IconButton>
+            <ArrowDropDownIcon />
+          </IconButton>
+          <IconButton>
+            <RedoIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
         </div>
       </div>
     </div>
