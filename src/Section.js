2a9e768ede1f88b4b176f998532729b5da7c5908
Kenneth Tran <48599140+kennethtran590@users.noreply.github.com>
2022-05-01 16:43:44 -0700

    Added styling for selected section
    Background will change to white smoke to indicate section selected

--- src/Section.js
+++ src/Section.js
@@ -2,13 +2,16 @@ import React from "react";
 import "./Section.css";
 
 function Section({ Icon, title, color, selected }) {
-  const divStyle = {
+  const sectionStyle = {
     borderBottom: `3px solid ${color}`,
     color: `${selected && color}`,
   };
 
   return (
-    <div style={divStyle} className="section">
+    <div
+      style={sectionStyle}
+      className={`section ${selected && "section--selected"}`}
+    >
       <Icon />
       <h4>{title}</h4>
     </div>
