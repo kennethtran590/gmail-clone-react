a6a608029a7bbcd2bfe24461f2558ecbd9142534
Kenneth Tran <48599140+kennethtran590@users.noreply.github.com>
2022-05-01 15:40:52 -0700

    Added icon buttons to the email list settings right hand side

--- src/EmailList.js
+++ src/EmailList.js
@@ -4,6 +4,10 @@ import { Checkbox, IconButton } from "@mui/material";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import RedoIcon from "@mui/icons-material/Redo";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import KeyboardHideIcon from "@mui/icons-material/KeyboardHide";
+import SettingsIcon from "@mui/icons-material/Settings";
 
 function EmailList() {
   return (
@@ -19,6 +23,20 @@ function EmailList() {
           </IconButton>
           <IconButton>
             <MoreVertIcon />
+          </IconButton>
+        </div>
+        <div className="emailList__settingsRight">
+          <IconButton>
+            <ChevronLeftIcon />
+          </IconButton>
+          <IconButton>
+            <ChevronRightIcon />
+          </IconButton>
+          <IconButton>
+            <KeyboardHideIcon />
+          </IconButton>
+          <IconButton>
+            <SettingsIcon />
           </IconButton>
         </div>
       </div>
