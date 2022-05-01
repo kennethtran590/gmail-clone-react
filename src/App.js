8d40b4b39f217505a362974499f154d934805a18
Kenneth Tran <48599140+kennethtran590@users.noreply.github.com>
2022-05-01 15:00:39 -0700

    Import React router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Sidebar />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <div className="app__body">
+          <Sidebar />
+          <Routes></Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
