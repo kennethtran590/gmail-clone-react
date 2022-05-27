d34f69c0396c2a449f4af94e4f43c7053764f78c
Kenneth Tran <48599140+kennethtran590@users.noreply.github.com>
2022-05-26 23:36:41 -0700

    Imported icons, useNavigate hook from react router, implemented email row.

--- src/EmailRow.js
+++ src/EmailRow.js
@@ -1,7 +1,44 @@
 import React from "react";
 import "./EmailRow.css";
+import { IconButton, Checkbox } from "@mui/material";
+import StarBorderOutlinedIcon from "@mui/icons-material/StarBorderOutlined";
+import LabelImportantOutlinedIcon from "@mui/icons-material/LabelImportantOutlined";
+import { useNavigate } from "react-router-dom";
+
 function EmailRow({ id, title, subject, description, time }) {
-  return <div className="emailRow"></div>;
+  const navigate = useNavigate();
+
+  return (
+    <div
+      onClick={() => {
+        navigate("/mail");
+      }}
+      className="emailRow"
+    >
+      <div className="emailRow__options">
+        <Checkbox />
+        <IconButton>
+          <StarBorderOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <LabelImportantOutlinedIcon />
+        </IconButton>
+      </div>
+
+      <div className="emailRow__title">
+        <h3>{title}</h3>
+      </div>
+
+      <div className="emailRow__message">
+        <h4>
+          {subject}{" "}
+          <span className="emailRow__description">- {description}</span>
+        </h4>
+      </div>
+
+      <p className="emailRow__time">{time}</p>
+    </div>
+  );
 }
 
 export default EmailRow;
